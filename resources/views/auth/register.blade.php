2e142784fbafc798a18d97f895390a7cb08b9f6e
golz04 <muhammadhaidar862@gmail.com>
2022-01-12 21:02:48 +0700

    responsive regis

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -84,75 +84,77 @@
 @section('auth-content')
 <div class="limiter">
     <div class="container-register100">
-      <img src="{{ asset('frontend/images/bg_regsiter1.svg') }}" alt="">
-      <div class="card-custom" style="
+      {{-- <div class="card-custom" style="
         padding: 2rem;
         position: absolute;  
         margin-left: auto;
         margin: auto;
         margin-right: auto;
         left: 38%;
-        right: 38%;">
-        <form  method="POST" action="{{ route('register') }}" class=" validate-form">
-          @csrf
-          <span class="login100-form-title">
-            Registrasi
-            <p>Lengkapi data diri anda</p>
-          </span>
-
-          <div class="wrap-input100 validate-input">
-            <input id="name" type="text" class="input100 @error('name') is-invalid @enderror" name="name" value="{{ old('name') }}" required autocomplete="name" autofocus  placeholder="Nama Lenkap">
-
-            @error('name')
-                <span class="invalid-feedback" role="alert">
-                    <strong>{{ $message }}</strong>
-                </span>
-            @enderror
-          </div>
-
-          <div class="wrap-input100 validate-input">
-            <input id="email" type="email" class="input100 @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email" placeholder="Email">
-
-            @error('email')
-                <span class="invalid-feedback" role="alert">
-                    <strong>{{ $message }}</strong>
-                </span>
-            @enderror
-          </div>
-
-          <div class="wrap-input100 validate-input">
-            <input id="telepon" type="telepon" class="input100 @error('telepon') is-invalid @enderror" name="telepon" required autocomplete="telepon" placeholder="No Telepon">
-
-            @error('telepon')
-                <span class="invalid-feedback" role="alert">
-                    <strong>{{ $message }}</strong>
-                </span>
-            @enderror
-          </div>
-
-          <div class="wrap-input100 validate-input">
-            <input id="password" type="password" class="input100 @error('password') is-invalid @enderror" name="password" required autocomplete="new-password" placeholder="Password">
-
-            @error('password')
-                <span class="invalid-feedback" role="alert">
-                    <strong>{{ $message }}</strong>
-                </span>
-            @enderror
-          </div>
-
-          <div class="wrap-input100 validate-input">
-            <input id="password-confirm" type="password" class="input100" name="password_confirmation" required autocomplete="new-password" placeholder="Konfirmasi Password">
-          </div>
-
-
-          <div class="container-login100-form-btn">
-            <button type="submit" class="login100-form-btn">
-              REGISTRASI
-            </button>
-          </div>
-        </form>
-      </div>
-      <img src="{{ asset('frontend/images/bg_regsiter2.svg') }}" alt="" srcset="">
+        right: 38%;"> --}}
+        <div class="row m-auto p-3 py-4" style="background-color: white; border-radius: 15px;">
+            <div class="col-12">       
+                <form  method="POST" action="{{ route('register') }}" class=" validate-form">
+                  @csrf
+                  <span class="login100-form-title">
+                    Registrasi
+                    <p>Lengkapi data diri anda</p>
+                  </span>
+        
+                  <div class="wrap-input100 validate-input">
+                    <input id="name" type="text" class="input100 @error('name') is-invalid @enderror" name="name" value="{{ old('name') }}" required autocomplete="name" autofocus  placeholder="Nama Lenkap">
+        
+                    @error('name')
+                        <span class="invalid-feedback" role="alert">
+                            <strong>{{ $message }}</strong>
+                        </span>
+                    @enderror
+                  </div>
+        
+                  <div class="wrap-input100 validate-input">
+                    <input id="email" type="email" class="input100 @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email" placeholder="Email">
+        
+                    @error('email')
+                        <span class="invalid-feedback" role="alert">
+                            <strong>{{ $message }}</strong>
+                        </span>
+                    @enderror
+                  </div>
+        
+                  <div class="wrap-input100 validate-input">
+                    <input id="telepon" type="telepon" class="input100 @error('telepon') is-invalid @enderror" name="telepon" required autocomplete="telepon" placeholder="No Telepon">
+        
+                    @error('telepon')
+                        <span class="invalid-feedback" role="alert">
+                            <strong>{{ $message }}</strong>
+                        </span>
+                    @enderror
+                  </div>
+        
+                  <div class="wrap-input100 validate-input">
+                    <input id="password" type="password" class="input100 @error('password') is-invalid @enderror" name="password" required autocomplete="new-password" placeholder="Password">
+        
+                    @error('password')
+                        <span class="invalid-feedback" role="alert">
+                            <strong>{{ $message }}</strong>
+                        </span>
+                    @enderror
+                  </div>
+        
+                  <div class="wrap-input100 validate-input">
+                    <input id="password-confirm" type="password" class="input100" name="password_confirmation" required autocomplete="new-password" placeholder="Konfirmasi Password">
+                  </div>
+        
+        
+                  <div class="container-login100-form-btn">
+                    <button type="submit" class="login100-form-btn">
+                      REGISTRASI
+                    </button>
+                  </div>
+                </form>
+            </div>
+        </div>
+      {{-- </div> --}}
     </div>
   </div>
 @endsection
